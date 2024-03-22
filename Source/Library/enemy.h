2df2585e9cc178ee5151b69c5f743ef472b0c3d9
111590039 <t111590039@ntut.org.tw>
2024-03-22 12:01:53 +0800

    tower update and map update

---
+++ Source/Library/enemy.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "stdafx.h"
+#include "gameutil.h"
+
+class enemy : public game_framework::CMovingBitmap {
+public:
+
+protected:
+
+private:
+	std::string type;
+	float count, health, speed, bounty, exp, probability;
+};
+
+
+
+/*int prob = 10 - (int)(value / 28);
+if (prob < 1) prob = 1;
+if (value > 300) prob = 0;
+return prob;*/
